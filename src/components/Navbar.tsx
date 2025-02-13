8c507eca9ebb190cfd57c2fa29a5bf0b7c1a7b54
ImaJin <imajinarts14@gmail.com>
2025-02-13 02:35:40 +0100

    updates

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -75,7 +75,7 @@ export const Navbar = () => {
               <SheetContent side={"left"}>
                 <SheetHeader>
                   <SheetTitle className="font-bold text-xl">
-                    Shadcn/React
+                    Python Cameroon
                   </SheetTitle>
                 </SheetHeader>
                 <nav className="flex flex-col justify-center items-center gap-2 mt-4">
