9e94f12c7e6e5c59b1565d1ae4e26398c9f89ed3
Steve Yonkeu <yokwejuste@yahoo.com>
2025-12-06 19:36:50 +0100

    feat: add Vite.js support with React SWC plugin and Dockerfile
    Signed-off-by: Steve Yonkeu <yokwejuste@yahoo.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,25 @@
-import path from "path";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import path from "path"
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      '@components': path.resolve(__dirname, './src/components'),
     },
   },
-});
+  server: {
+    host: true,
+    port: 5173,
+    allowedHosts: true,
+  },
+  preview: {
+    host: true,
+    port: 3000,
+    strictPort: true,
+    allowedHosts: true,
+  },
+})
