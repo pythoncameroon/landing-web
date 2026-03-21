962ec3a2c113fca58fe8cea2b30e0e1696a66b8c
EssiJunior <nessipjunior@gmail.com>
2026-03-21 11:03:48 +0100

    Updated font

--- tailwind.config.js
+++ tailwind.config.js
@@ -80,6 +80,10 @@ module.exports = {
                 "accordion-down": "accordion-down 0.2s ease-out",
                 "accordion-up": "accordion-up 0.2s ease-out",
             },
+            fontFamily: {
+                dotgothic16: ["var(--font-dotgothic16)", "sans-serif"],
+                "space-mono": ["var(--font-space-mono)", "monospace"],
+            },
         },
     },
     plugins: [require("tailwindcss-animate")],
